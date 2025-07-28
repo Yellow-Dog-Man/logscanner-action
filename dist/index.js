b19d44078da4015ef4c4bdeef33e4bed558f28b8
Jae J4 Lo Presti <j@j4.lc>
2025-07-29 02:46:14 +0300

    chore#: update action dist files

--- dist/index.js
+++ dist/index.js
@@ -50339,11 +50339,7 @@ function checkModLoader(logContent) {
 }
 
 function checkForCleanExit(logContent) {
-    const lines = logContent.split('\n');
-    
-    let isClean = lines.at(-1) === "<<< LOG END >>>";
-
-    return isClean;
+    return logContent.includes("<<< LOG END >>>");
 }
 
 function parseResoniteLogContent(logContent) {
