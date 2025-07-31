d1471500f88824970d4dbead127c52aed592e1db
Jae J4 Lo Presti <j@j4.lc>
2025-07-31 19:02:39 +0300

    fix#: fix ternary one more time (i forgor) (#3)

--- src/index.js
+++ src/index.js
@@ -118,7 +118,7 @@ function formatMarkdownMessage(data) {
             r.pcSpecs.vram,
             r.pcSpecs.memory,
             r.headset,
-            r.modLoader.isLoaded ? "No" : "Yes",
+            r.modLoader.isLoaded ? "Yes" : "no",
             r.cleanExit ? "✅" : "❌",
         ]);
 
